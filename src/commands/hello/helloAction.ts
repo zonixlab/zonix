7b7cada2d01b0a14ae1b735fea4ce1c9d6ffc49f
Bruno Martins <brunomart97@hotmail.com>
2023-03-19 11:37:55 -0300

    feat: color setting for the CLI

--- src/commands/hello/helloAction.ts
+++ src/commands/hello/helloAction.ts
@@ -1,13 +1,16 @@
 import type { GeneralOptions } from '../../typings/General'
+import kleur from 'kleur'
 
 export const helloAction = (options: GeneralOptions) => {
   const name = options[Object.keys(options)[0]]
 
   if (!name) {
     return console.log(
-      `Greetings, little Jedi. May the force be with you!\n\n* use --name or -n to declare your name and get a greeting `
+      `Greetings, little ${kleur.green(
+        'Jedi'
+      )}. May the force be with you!\n\n* use --name or -n to declare your name and get a greeting `
     )
   }
 
-  console.log(`Hello, ${name}!`)
+  console.log(`Hello, ${kleur.green(name)}!`)
 }
